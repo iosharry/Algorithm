2394bc99880346b2c1369d6537318a3d6de34e2e
gwangyonglee <gwangyonglee92@gmail.com>
2018-01-25 23:55:38 +0900

    별찍기2

---
+++ Beakjoon2439/Beakjoon2439/main.swift
@@ -0,0 +1,19 @@
+
+import Foundation
+
+let a = readLine()
+
+if let a = a {
+    
+    if 1 <= Int(a)! && Int(a)! <= 100 {
+        for i in 1 ... Int(a)! {
+            for _ in stride(from: Int(a)! - i , to: 0, by: -1) {
+                print(" ", terminator: "")
+            }
+            for _ in 1 ... i {
+                print("*", terminator: "")
+            }
+            print("")
+        }
+    }
+}
