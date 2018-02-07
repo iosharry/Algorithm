42fea98f29dbcfaba3b338fc18535c2a2d62b0f9
gwangyonglee <gwangyonglee92@gmail.com>
2018-02-07 22:38:52 +0900

    단어의 개수

---
+++ Beakjoon1152/Beakjoon1152/main.swift
@@ -0,0 +1,33 @@
+/*
+ *  Beakjoon 1152번 문제 - 단어의 개수
+ */
+
+import Foundation
+
+class beakjoon1152 {
+    
+    func solution() {
+        let a = readLine()
+        
+        if let a = a {
+            var input = a.components(separatedBy: " ")
+            var arr = [String]()
+            
+            for i in 0 ..< input.count {
+                if input[i] != "" {
+                    arr.append(input[i])
+                }
+            }
+            print(arr.count)
+        }
+    }
+}
+
+let customclass = beakjoon1152()
+customclass.solution()
+
+
+
+
+
+
